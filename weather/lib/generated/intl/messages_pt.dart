016ef86f0c738ba890434b18972d639a28474cc2
Samuel Tavares <spontopt@gmail.com>
2022-02-20 20:00:51 +0000

    final version

---
+++ weather/lib/generated/intl/messages_pt.dart
@@ -0,0 +1,50 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a pt locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'pt';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "Precipitation": MessageLookupByLibrary.simpleMessage("\nPrecipitação"),
+        "Pressure": MessageLookupByLibrary.simpleMessage("\nPressão"),
+        "brokenClouds": MessageLookupByLibrary.simpleMessage("Nuvens"),
+        "clear": MessageLookupByLibrary.simpleMessage("Céu Limpo"),
+        "clearSky": MessageLookupByLibrary.simpleMessage("Céu Limpo"),
+        "clouds": MessageLookupByLibrary.simpleMessage("Nuvens"),
+        "drizzle": MessageLookupByLibrary.simpleMessage("Chuviscos"),
+        "error": MessageLookupByLibrary.simpleMessage("Erro!"),
+        "fewClouds": MessageLookupByLibrary.simpleMessage("Poucas Nuvens"),
+        "humidity": MessageLookupByLibrary.simpleMessage("\nHumidade"),
+        "infoUnavailable": MessageLookupByLibrary.simpleMessage(
+            "Ainda não existe informação disponível\n para mostrar!"),
+        "locationPermissions": MessageLookupByLibrary.simpleMessage(
+            "Permissões de localização não permitidas!"),
+        "locationService": MessageLookupByLibrary.simpleMessage(
+            "Serviço de localização não disponível! Atualize!"),
+        "mist": MessageLookupByLibrary.simpleMessage("Nublado"),
+        "rain": MessageLookupByLibrary.simpleMessage("Chuva"),
+        "refresh": MessageLookupByLibrary.simpleMessage("Atualizar"),
+        "scatteredClouds": MessageLookupByLibrary.simpleMessage("Nuvens dispersas"),
+        "showerRain": MessageLookupByLibrary.simpleMessage("Nuvens"),
+        "snow": MessageLookupByLibrary.simpleMessage("Neve"),
+        "thunderstorm": MessageLookupByLibrary.simpleMessage("Tempestade"),
+        "windSpeed": MessageLookupByLibrary.simpleMessage("\nVento")
+      };
+}
